636e3e60549fc2e1715c234a958875c9bc68cb0a
Brandy Foster <brandy.foster@detroitlabs.com>
2014-09-13 14:14:19 -0400

    Added comments and strings for program.

--- com/sample/calculatorapp/CalculatorApp.java
+++ com/sample/calculatorapp/CalculatorApp.java
@@ -3,8 +3,39 @@ package com.sample.calculatorapp;
 /**
  * Created by BFineRocks on 9/12/14.
  */
+import java.util.Scanner;
+
+/**
+ * This Calculator App will take in user input and perform the requested operations.
+ * The user will input the operation (addition, subtraction, multiplication, division, or square root)
+ * they would like to perform, then 1 or 2 digits and the app will output the answer.
+ * After displaying the answer, the app will then ask the user if they would like to perform another operation. If so,
+ * the app will begin again. If not, it will stop. It will include error handling for bad input using a method.
+ */
+
 public class CalculatorApp {
     static public void main(String[] args){
+        String add, subtract, multiply, divide, welcome, requestForOperation, options, requestForDigits;
+        Scanner userInput;
+
+        welcome = "Welcome to the AutoCalc 3000 Bfinity Edition.";
+        requestForOperation = "Please select your requested operation by typing the choice in below.";
+        options = "Addition, Subtraction, Multiplication, Division, Square Root";
+        requestForDigits = "Please enter digits you are calculating. (You must enter at least one)";
+
         
+
+
+
+
+
     }
 }
+
+
+
+
+
+
+
+
